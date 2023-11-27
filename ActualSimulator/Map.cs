3b5be5cbe0d8cd12aadf4adbc8dd01be01203c48
eiglerl <eiglerlukas@gmail.com>
2023-11-27 17:31:02 +0100

    added GetNodeByID function

---
+++ ActualSimulator/Map.cs
@@ -0,0 +1,16 @@
+﻿namespace ActualSimulator;
+using FantomMapLibrary;
+
+public class Map : IMap<Node> 
+
+{
+    public Map(List<Node> nodes)
+    {
+        Nodes = nodes;
+    }
+
+    public List<Node> Nodes { get; init; }
+    public Node GetNodeByID(int id)
+        => Nodes[id - 1];
+    
+}
